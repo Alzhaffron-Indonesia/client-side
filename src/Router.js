f082dfacc1194d168760d7c7042d1d61145f083e
alfi2811 <alfinasution64@gmail.com>
2020-11-14 22:35:16 +0700

    fix netlify bug8

--- src/Router.js
+++ src/Router.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Landing, Login, Register, RegisterReseller, Store } from './Containers/Index';
-// import { Provider as ReduxProvider } from 'react-redux';
-// import configStore from './Modules/Store';
+import { Provider as ReduxProvider } from 'react-redux';
+import configStore from './Modules/Store';
 import './Modules/index.scss';
 import './Modules/fonts.scss';
 
-// const reduxStore = configStore(window.REDUX_INITIAL_DATA)
+const reduxStore = configStore(window.REDUX_INITIAL_DATA)
 const ROUTES = require('./Constants/Routes');
 
 const Router = () => {
     return (
         <div className="font-lato">
-            {/* <ReduxProvider store={reduxStore}> */}
+            <ReduxProvider store={reduxStore}>
             <BrowserRouter>
                 <Switch>
-                    <Route exact path="/" component={Login} />
-                    <Route exact path="/login" component={Landing} />
-                    <Route exact path="/register" component={Register} />
+                    <Route exact path={ROUTES.LANDING} component={Landing} />
+                    <Route exact path={ROUTES.LOGIN} component={Login} />
+                    <Route exact path={ROUTES.REGISTER} component={Register} />
                     <Route exact path={ROUTES.REGISTERRESELLER} component={RegisterReseller} />
                     <Route exact path={ROUTES.STORE} component={Store} />
                 </Switch>
             </BrowserRouter>
-            {/* </ReduxProvider> */}
+            </ReduxProvider>
         </div>
     )
 }
