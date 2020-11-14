541dbd0031d43a524f57f69bb22cfe85d22fb222
alfi2811 <alfinasution64@gmail.com>
2020-11-14 22:10:13 +0700

    fix netlify bug2

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Landing, Login, Register, RegisterReseller, Store } from './Containers/Index';
 import { Provider as ReduxProvider } from 'react-redux';
-import configStore from './Modules/Redux/Store';
+import configStore from './Modules/Store';
 import './Modules/index.scss';
 import './Modules/fonts.scss';
 
